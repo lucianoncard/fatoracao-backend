ad8be18ea2bf07a746a8939d5d903a87ef72bd46
lucianoncard <68202654+lucianoncard@users.noreply.github.com>
2021-03-14 16:48:38 -0300

    Add files via upload

---
+++ src/app/controllers/QuestaoController.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('../../config/upload');
+
+const User = require('../models/User');
+const Questao = require('../models/Questao');
+
+const router = express.Router();
+const upload = multer(uploadConfig);
+  
+router.get('/', async (req, res) => {
+  const { palavra } = req.headers;
+  var spots = '';
+  if ( palavra != 'Selecione') { spots = await Questao.find({ vestibular: palavra }); }
+  else { spots = await Questao.find(); }
+  return res.json(spots);
+});
+
+router.post('/', upload.single('thumbnail'), async (req, res) => {
+  const { filename } = req.file;
+  const { vestibular, ano, questao } = req.body;
+  const { aluno_id } = req.headers;
+  const user = await User.findById(aluno_id);
+  if (!user){
+    return res. status(400).json({ error: 'Usuário não existe!' });
+  }
+  const spot = await Questao.create({
+    user: aluno_id,
+    thumbnail: filename,
+    vestibular,
+    ano,
+    questao
+  });
+  return res.json(spot);
+});
+
+module.exports = app => app.use('/questoes', router);
