2feaa5e547a10ba88f84440f42a42f49061a6b10
lucianoncard <68202654+lucianoncard@users.noreply.github.com>
2021-03-20 21:05:05 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./app/controllers/index')(app);
+
+app.use('/files', express.static(path.resolve(__dirname, 'uploads')));
+
+app.listen(process.env.PORT || 3333);
